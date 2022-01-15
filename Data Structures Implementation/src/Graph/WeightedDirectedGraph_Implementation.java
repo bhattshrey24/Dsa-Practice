f871f111a7024faa78c9b2a557991e0c58c2c948
Shrey Bhatt <bhatt.shrey1224@gmail.com>
2022-01-15 16:42:28 +0530

    Modified Directed and Undirected Graph

--- Data Structures Implementation/src/Graph/WeightedDirectedGraph_Implementation.java
+++ Data Structures Implementation/src/Graph/WeightedDirectedGraph_Implementation.java
@@ -12,14 +12,11 @@ public class WeightedDirectedGraph_Implementation {
 		adj = new LinkedList[v];// we are initializing adj array , we are just telling its size
 
 		for (int i = 0; i < v; i++) { // assuming vertices starts from 0
+			
 			adj[i] = new LinkedList<Edge>();// we are connecting a empty linked list to every vertex i.e element of
 											// array adj
 		}
 	}
-
-//	 public WeightedDirectedGraph(int v) {
-//		// TODO Auto-generated constructor stub
-//	}
 
 	class Edge { // simply make linked list of this class instead of Integer if your graph is
 					// weighted it will look like a[1]=>(v:2,w:8)->(v:4,w:5)
@@ -33,12 +30,11 @@ public class WeightedDirectedGraph_Implementation {
 	}
 
 	public void addEdge(int source, int destination, int weight) {
-		// creating an undirected graph therefore i connected source with destination as
-		// well as destination with source
 		adj[source].add(new Edge(destination, weight));// simply adding the a node in linkedlist of vertice 'source'
 	}
 
 	public void displayGraph() {
+		
 		for (int i = 0; i < adj.length; i++) {
 			LinkedList<Edge> temp = adj[i];
 			int j = 0;
