45dfd0e228c866f703e70867210ea2dd11b400fc
Shrey Bhatt <bhatt.shrey1224@gmail.com>
2022-02-03 18:09:28 +0530

    fixing old code

--- Dsa(Pep Coding Course)/src/SlidingPuzzle773.java
+++ Dsa(Pep Coding Course)/src/SlidingPuzzle773.java
@@ -8,17 +8,19 @@ public class SlidingPuzzle773 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[][] board = { { 4, 1, 2 }, { 5, 0, 3 } };
-		int ans=slidingPuzzle(board);
-		
-		System.out.println("sol is : "+ans);
+		int ans = slidingPuzzle(board);
+
+		System.out.println("sol is : " + ans);
 	}
 
 	public static int slidingPuzzle(int[][] board) {
-        // Step 1 make arraylist of possible positions where 0 can go without going out of bound if it is currently at position i  
+		// Step 1 make arraylist of possible positions where 0 can go without going out
+		// of bound if it is currently at position i
 		ArrayList<Integer>[] ar = new ArrayList[6];
 		// possible places 0 can move if its at position ar[i]
 		for (int i = 0; i < ar.length; i++) {
-			ar[i] = new ArrayList<>(); // just initializing array list basically putting an empty arraylist at every index
+			ar[i] = new ArrayList<>(); // just initializing array list basically putting an empty arraylist at every
+										// index
 		}
 		ar[0].add(1);
 		ar[0].add(3);
@@ -35,7 +37,8 @@ public class SlidingPuzzle773 {
 		ar[5].add(2);
 		ar[5].add(4);
 
-		//Step 2 converting given board into linear string using concept of converting 2d matrix to linear matrix using cell number
+		// Step 2 converting given board into linear string using concept of converting
+		// 2d matrix to linear matrix using cell number
 		String givenBoard = "";
 		int initialPosOf0 = 0;
 		for (int i = 0; i < board.length; i++) {
@@ -45,7 +48,7 @@ public class SlidingPuzzle773 {
 					initialPosOf0 = i * board[i].length + j;
 				}
 				givenBoard = givenBoard + String.valueOf(board[i][j]);
-			
+
 			}
 		}
 		String requiredBoard = "123450"; // that is the required pattern to be found
@@ -55,33 +58,34 @@ public class SlidingPuzzle773 {
 		int min = 0;
 		ArrayDeque<Pair> que = new ArrayDeque<>();
 		que.add(new Pair(givenBoard, 0, initialPosOf0));
-		
-		boolean permutaionPossible =false;
+
+		boolean permutaionPossible = false;
 		while (!que.isEmpty()) {
 			Pair currEle = que.remove();
-			
+
 			if (vis.contains(currEle.permu)) { // check visited
 				continue;
 			}
-			
+
 			vis.add(currEle.permu); // mark visited
 
 			if (currEle.permu.equals(requiredBoard)) { // work
 				min = currEle.cost;
-				permutaionPossible=true;
+				permutaionPossible = true;
 				break;
 			}
 
-			for (int pponz : ar[currEle.posOfZero]) { // ppoz=possible position of next 0
-				// System.out.println(el);
+			for (int pponz : ar[currEle.posOfZero]) { // pponz=possible position of next 0
 				String st = "";
 				char a, b;
 				if (pponz < currEle.posOfZero) { // this is the case when current position of 0 > next position of 0
 					a = currEle.permu.charAt(pponz);
-					// char a=(char)Integer.toString(curr.).charAt(0);
-					b = currEle.permu.charAt(currEle.posOfZero);// i.e 0
+					b = currEle.permu.charAt(currEle.posOfZero);// i.e current position of 0
 					st = currEle.permu.substring(0, pponz) + b + currEle.permu.substring(pponz + 1, currEle.posOfZero)
-							+ a + currEle.permu.substring(currEle.posOfZero + 1);
+							+ a + currEle.permu.substring(currEle.posOfZero + 1); // basically swapping current zeros
+																					// position with pponz so that 0
+																					// goes to pponz and element at pponz
+																					// goes to current position of 0
 				} else {// this is the case when current position of 0 < next position of 0
 					b = currEle.permu.charAt(pponz);
 					a = currEle.permu.charAt(currEle.posOfZero);
@@ -90,18 +94,18 @@ public class SlidingPuzzle773 {
 							+ currEle.permu.substring(pponz + 1);
 				}
 
-				if(!vis.contains(st)) { // simply checking if it is visited or not and adding it to que if not visited
-					que.add(new Pair(st,currEle.cost+1,pponz));
+				if (!vis.contains(st)) { // simply checking if it is visited or not and adding it to que if not visited
+					que.add(new Pair(st, currEle.cost + 1, pponz));
 				}
-			
+
 			}
 		}
-		if(permutaionPossible) {
+		if (permutaionPossible) {
 			return min;
-			
-		}else {
+
+		} else {
 			return -1;
-			
+
 		}
 	}
 
